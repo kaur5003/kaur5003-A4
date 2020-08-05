6f5072c6e67687ad897b7d04ae7ff1e6f14221e2
kaur5003 <46930469+kaur5003@users.noreply.github.com>
2020-08-05 00:12:03 -0400

    Add files via upload

---
+++ assets/data/myInfo.ts
@@ -0,0 +1,10 @@
+import {Kaur5003} from '../../app/kaur5003';
+
+export const KAUR5003 : Kaur5003 = {
+    id : 991512152,
+    Name : 'Harjit Kaur',
+    LoginName: 'kaur5003',
+    campus: 'Davis',
+    title: 'Assignment-4'
+
+};
